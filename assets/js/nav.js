e159c899b6955214213be244f74db085309d7c25
Jorge Garate <jorgeluis.garate@gmail.com>
2023-01-06 20:34:24 -0300

    responsive nav desde 30px al full

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,17 +3,20 @@ const sider = document.querySelector('.sidebar-panel');
 const viewWrapper = document.querySelector('.view-wrapper')
 const dropdownTrigger = document.querySelector('.dropdown-trigger')
 const closeBtn = document.querySelector('.close')
+const colorNav =document.querySelector('.color-nav')
 let menuOpen = false;
 let dropdown= false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         sider.classList.add('active');
+        colorNav.classList.add('active');
         viewWrapper.classList.add('is-pushed-full');
         menuOpen=true;  
     }
     else {
         sider.classList.remove('active');
         viewWrapper.classList.remove('is-pushed-full');
+        colorNav.classList.remove('active');
         menuOpen=false;
     }
 
@@ -29,7 +32,7 @@ menuBtn.addEventListener('click', () => {
 }
   });
 closeBtn.addEventListener('click', () => {
-
+    colorNav.classList.remove('active');
     sider.classList.remove('active');
     viewWrapper.classList.remove('is-pushed-full');
     menuOpen=false;
